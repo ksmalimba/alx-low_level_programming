62b94a6b47215e351713472ade383faa5488b2a4
ksmalimba <ksmalimba@gmail.com>
2022-09-13 14:14:13 -0700

    k

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,14 +5,16 @@
  */
 int main(void)
 {
-	int n = 0;
-	char str_s[8] = "_putchar";
-
-	while (n < 9)
-	{
-		_putchar(str_s[n]);
-		n++;
-	}
+	_putchar('A');
+	_putchar('n');
+	_putchar('y');
+	_putchar('t');
+	_putchar('h');
+	_putchar('i');
+	_putchar('n');
+	_putchar('g');
+	_putchar('n');
 	_putchar('\n');
+
 	return (0);
 }
